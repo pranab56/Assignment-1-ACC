929a5b9cb9f41b6c723286e04f65798ff350f47c
pranab56 <pronabhalder53@gmail.com>
2022-12-29 01:29:23 +0600

    delete vailed id done

--- controller/getUser.js
+++ controller/getUser.js
@@ -1,16 +1,20 @@
 let user = require("../data.json");
 
+
 module.exports.randomUser = (req, res) => {
   const values = Object.values(user);
   const randomValue = values[parseInt(Math.random() * values.length)];
   res.send(randomValue);
 };
 
+
 module.exports.allUser = (req, res) => {
   const { s } = req.query;
   const result = user.slice(0, s);
   res.send(result);
 };
+
+
 module.exports.postUser = (req, res) => {
   const data = req.body;
   if (
@@ -28,21 +32,26 @@ module.exports.postUser = (req, res) => {
   }
 };
 
+
 module.exports.UpdateUser = (req, res) => {
   const { id } = req.params;
-  const {contact}=req.body;
-    const updateUser = user.find((person) => person.id === Number(id));
-    if(!updateUser){
-        return res.send("please valid id")
-    }
-    else{
-        updateUser.contact =contact;
-        return res.send(updateUser);
-    }
- 
+  const { contact } = req.body;
+  const updateUser = user.find((person) => person.id === Number(id));
+  if (!updateUser) {
+    return res.send("please valid id");
+  } else {
+    updateUser.contact = contact;
+    return res.send(updateUser);
+  }
 };
+
+
 module.exports.deleteUser = (req, res) => {
-    const {id}=req.params;
-    const deletePerson=user.filter(p=>p.id != id);
-    res.send(deletePerson)
+  const { id } = req.params;
+  const findId = user.find((p) => p.id == id);
+  if (!findId) {
+    return res.send("please vaild a id");
+  }
+  const deletePerson = user.filter((p) => p.id != id);
+  res.send(deletePerson);
 };
